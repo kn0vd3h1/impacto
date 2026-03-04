6a3cdaff8c30ae14688835e37769d7b39f35b7ff
Enorovan <enorovan@gmail.com>
2026-03-04 21:38:07 +0100

    game-exit: [C;CLCC] make Exit button call SysMesExit logic

--- src/games/cclcc/titlemenu.cpp
+++ src/games/cclcc/titlemenu.cpp
@@ -12,6 +12,7 @@
 #include "../../profile/game.h"
 #include "../../vm/interface/input.h"
 #include "../../audio/audiosystem.h"
+#include "../../profile/scriptinput.h"
 
 namespace Impacto {
 namespace UI {
@@ -137,7 +138,11 @@ TitleMenu::TitleMenu() {
         5, ExitSprite, ExitSprite, ItemHighlightSprite,
         glm::vec2(ItemHighlightOffsetX, (ItemYBase + (5 * ItemPadding))));
     setupBtn(
-        Exit, [](auto*) { Game::ShouldQuit = true; }, MainItems, FDIR_DOWN);
+        Exit,
+        [](auto*) {
+          Input::KeyboardButtonWentDown[SDL_SCANCODE_ESCAPE] = true;
+        },
+        MainItems, FDIR_DOWN);
   }
 
   // Load secondary Continue menu button
@@ -293,9 +298,11 @@ void TitleMenu::UpdateInput(float dt) {
     }
   }
   if (CurrentSubMenu && !CurrentSubMenu->HasFocus) return;
-  if (CurrentSubMenu || MainItems->HasFocus) {
+  if (IsFocused) {
     Menu::UpdateInput(dt);
-    MainItems->UpdateInput(dt);
+    if (MainItems->HasFocus && !CurrentSubMenu) {
+      MainItems->UpdateInput(dt);
+    }
     if (CurrentSubMenu) {
       CurrentSubMenu->UpdateInput(dt);
     }
@@ -443,6 +450,9 @@ void TitleMenu::ReturnToMenuUpdate() {
 }
 
 void TitleMenu::MainMenuUpdate() {
+  if (IsFocused) {
+    MainItems->HasFocus = true;
+  }
   MainItems->Tint.a =
       glm::smoothstep(0.0f, 1.0f, PrimaryFadeAnimation.Progress);
   ContinueItems->Tint.a =
